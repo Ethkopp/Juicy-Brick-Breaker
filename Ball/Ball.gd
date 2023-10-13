21c74e49477b9080b041bd5464260826e37b019d
Ethkopp <142931620+Ethkopp@users.noreply.github.com>
2023-10-12 21:21:05 -0800

    Completes Project

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -4,8 +4,24 @@ var min_speed = 100.0
 var max_speed = 600.0
 var speed_multiplier = 1.0
 var accelerate = false
+var decay = 0.02
+
+var time_highlight = 0.4
+var time_highlight_size = 0.3
 
 var released = true
+
+var wobble_period = 0.0
+var wobble_amplitude = 0.0
+var wobble_max = 5
+var wobble_direction = Vector2.ZERO
+var decay_wobble = 0.15
+
+var tween
+
+var distort_effect = 0.0002
+
+var h_rotate = 0.0
 
 var initial_velocity = Vector2.ZERO
 
@@ -24,6 +40,28 @@ func _on_Ball_body_entered(body):
 	if body.has_method("hit"):
 		body.hit(self)
 		accelerate = true	
+		$Images/Highlight.modulate.a = 1.0
+	if tween:
+		tween.kill()
+	tween = create_tween().set_parallel(true)
+	$Images/Highlight.modulate.a = 1.0
+	tween.tween_property($Images/Highlight, "modulate:a", 0, time_highlight).set_trans(Tween.TRANS_LINEAR)
+	$Images/Highlight.scale = Vector2(2.0,2.0)
+	tween.tween_property($Images/Highlight, "scale", Vector2(1.0,1.0),time_highlight_size).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN)
+	wobble_direction = linear_velocity.orthogonal().normalized()
+	wobble_amplitude = wobble_max
+
+func wobble():
+	wobble_period += 1
+	if wobble_amplitude > 0:
+		var pos = wobble_direction * wobble_amplitude * sin(wobble_period) 
+		$Images.position = pos
+		wobble_amplitude -= decay_wobble
+
+func distort():
+	var direction = Vector2(1 + linear_velocity.length() * distort_effect, 1 - linear_velocity.length() * distort_effect)
+	$Images.rotation = linear_velocity.angle()
+	$Images.scale = direction
 
 func _input(event):
 	if not released and event.is_action_pressed("release"):
@@ -35,6 +73,8 @@ func _integrate_forces(state):
 		var paddle = get_node_or_null("/root/Game/Paddle_Container/Paddle")
 		if paddle != null:
 			state.transform.origin = Vector2(paddle.position.x, paddle.position.y - 30)	
+		if $Images/Highlight.modulate.a > 0:
+			$Images/Highlight.modulate.a -= decay
 
 	if position.y > Global.VP.y + 100:
 		die()
@@ -49,11 +89,22 @@ func _integrate_forces(state):
 		state.linear_velocity = state.linear_velocity.normalized() * max_speed * speed_multiplier
 
 func change_size(s):
-	$ColorRect.scale = s
 	$CollisionShape2D.scale = s
 
 func change_speed(s):
 	speed_multiplier = s
 
+func comet():
+	h_rotate = wrapf(h_rotate+0.01, 0, 1)
+	var comet_container = get_node_or_null("/root/Game/Comet_Container")
+	if comet_container != null:
+		var sprite = $Images/Sprite.duplicate()
+		sprite.global_position = global_position
+		sprite.modulate.s = 0.6
+		sprite.modulate.h = h_rotate
+		comet_container.add_child(sprite)
+
 func die():
+	var die_sound = get_node("/root/Game/Die_Sound")
+	die_sound.play()
 	queue_free()
--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -6,6 +6,11 @@ var width = 0
 var width_default = 0
 var decay = 0.02
 
+var time_highlight = 0.4
+var time_highlight_size = 0.3
+
+var tween
+
 func _ready():
 	width = $CollisionShape2D.get_shape().size.x
 	width_default = width
@@ -16,13 +21,39 @@ func _physics_process(_delta):
 	position = target
 	for c in $Powerups.get_children():
 		c.payload()
+	if $Images/Highlight.modulate.a > 0:
+		$Images/Highlight.modulate.a -= decay
+	var ball_container = get_node_or_null("/root/Game/Ball_Container")
+	if ball_container != null and ball_container.get_child_count() > 0:
+		var ball = ball_container.get_child(0)
+		$Eye1/Pupil/Sprite.position.x = 7
+		$Eye2/Pupil/Sprite.position.x = 7
+		$Eye1/Pupil.look_at(ball.position)
+		$Eye2/Pupil.look_at(ball.position)
+		var d = ((($Mouth.global_position.y - ball.global_position.y)/Global.VP.y)-0.2)*2
+		d = clamp(d, -1, 1)
+		$Mouth.scale.y = d
+	else:
+		$Eye1/Pupil/Sprite.position.x = 0
+		$Eye2/Pupil/Sprite.position.x = 0
+		$Mouth.scale.y = 1
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		target.x += event.relative.x
 
 func hit(_ball):
-	pass
+	var Paddle_Sound = get_node("/root/Game/Paddle_Sound")
+	Paddle_Sound.play()
+	$Images/Highlight.modulate.a = 1.0
+	$Confetti.emitting = true
+	if tween:
+		tween.kill()
+	tween = create_tween().set_parallel(true)
+	$Images/Highlight.modulate.a = 1.0
+	tween.tween_property($Images/Highlight, "modulate:a", 0, time_highlight)
+	$Images/Highlight.scale = Vector2(1.5, 1.5)
+	tween.tween_property($Images/Highlight, "scale", Vector2(1,1), time_highlight_size).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN)
 
 func powerup(payload):
 	for c in $Powerups.get_children():
